acf0da81cd7dc15cb18082e3c046895536bfb643
johnroma <login@john.ro>
2026-01-04 22:26:05 +0100

    feat: react query wrapper

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,30 +1,62 @@
 /// <reference types="vite/client" />
-import type { ReactNode } from 'react'
 import {
-  Outlet,
-  createRootRoute,
   HeadContent,
+  Outlet,
   Scripts,
-} from '@tanstack/react-router'
+  createRootRouteWithContext,
+} from "@tanstack/react-router"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
+import * as React from "react"
+import type { QueryClient } from "@tanstack/react-query"
+import { DefaultCatchBoundary } from "~/components/DefaultCatchBoundary"
+import { NotFound } from "~/components/NotFound"
+import appCss from "~/styles/app.css?url"
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
   head: () => ({
     meta: [
       {
-        charSet: 'utf-8',
+        charSet: "utf-8",
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'IPTV Channels',
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
     ],
     links: [
-      { rel: 'stylesheet', href: '/src/styles/app.css' },
+      { rel: "stylesheet", href: appCss },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+      { rel: "icon", href: "/favicon.ico" },
     ],
   }),
+  errorComponent: (props) => {
+    return (
+      <RootDocument>
+        <DefaultCatchBoundary {...props} />
+      </RootDocument>
+    )
+  },
+  notFoundComponent: () => <NotFound />,
   component: RootComponent,
 })
 
@@ -36,14 +68,16 @@ function RootComponent() {
   )
 }
 
-function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+function RootDocument({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <HeadContent />
       </head>
       <body>
         {children}
+        <TanStackRouterDevtools position="bottom-right" />
+        <ReactQueryDevtools buttonPosition="bottom-left" />
         <Scripts />
       </body>
     </html>
